c188aeb60f7c777c53b1d22ed47bf329993227df
Olivier Fournier <foo_foo_ch@yahoo.fr>
2019-11-24 16:47:00 +0100

    Fix Kubernetes version to 1.15.4

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -57,7 +57,7 @@ EOF
 
 # Install Kubernetes
 echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
-yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
+yum install -y -q kubeadm-1.15.4 kubelet-1.15.4 kubectl-1.15.4 >/dev/null 2>&1
 
 # Start and Enable kubelet service
 echo "[TASK 10] Enable and start kubelet service"
--- vagrant-provisioning/bootstrap_kmaster.sh
+++ vagrant-provisioning/bootstrap_kmaster.sh
@@ -2,7 +2,18 @@
 
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=172.42.42.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
+# For flannel network
+#kubeadm init \
+#  --kubernetes-version '1.15.4' \
+#  --apiserver-advertise-address=172.42.42.100 \
+#  --pod-network-cidr=10.244.0.0/16 \
+#  >>/root/kubeinit.log 2>/dev/null
+# For calico network
+kubeadm init \
+  --kubernetes-version '1.15.4' \
+  --apiserver-advertise-address=172.42.42.100 \
+  --pod-network-cidr=192.168.0.0/16 \
+  >>/root/kubeinit.log 2>/dev/null
 
 # Copy Kube admin config
 echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
@@ -11,8 +22,12 @@ cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Deploy flannel network
-echo "[TASK 3] Deploy flannel network"
-su - vagrant -c "kubectl create -f /vagrant/kube-flannel.yml"
+#echo "[TASK 3] Deploy flannel network"
+#su - vagrant -c "kubectl create -f /vagrant/kube-flannel.yml"
+
+# Deploy calico network
+echo "[TASK 3] Deploy calico network"
+su - vagrant -c "kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml"
 
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
